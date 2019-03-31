167be150fa1d73b4e7aa5dcd4c1c98275867c055
Arihant Jain <aridj456@gmail.com>
2019-03-31 13:51:44 +0530

    adding comments

--- SB.cpp
+++ SB.cpp
@@ -1,55 +1,74 @@
 #include <iostream>
 #include <pthread.h>
 #include <semaphore.h>
-#include <unistd.h> 
+#include<random>
+#include <unistd.h>
+
 using namespace std;
 
-#define SEATS 20
+int seats;
+int empty;
 
+// semaphores
+
+// for barber availability
 sem_t s_barber;
-sem_t s_seats;
-sem_t s_customer;
-int empty = SEATS;
 
+// for customers
+sem_t s_customer;
+
+// for seats
+sem_t s_seats;
+
+// barber code
 void *barber(void *arg){
-    while(true){
-        sem_wait(&s_customer);
-        sem_wait(&s_seats);
-        empty++;
-        sem_post(&s_barber);
-        sem_post(&s_seats);
-        cout <<"\nCustomer hair cuts"<< endl;
-        sleep(3);
-    }
+  while(1){
+    sem_wait(&s_customer);
+    sem_wait(&s_seats);
+    empty++;
+    sem_post(&s_barber);
+    sem_post(&s_seats);
+    cout <<"\nBarber cutting hair"<< endl;
+    sleep(rand()%5);
+    // barber takes some time for cutting (here it is simulated by random number from 0 to 4)
+  }
 }
 
+// customer code
 void *customer(void *arg){
-    while(true){
-        sem_wait(&s_seats);
-        if(empty > 0){
-            empty--;
-            sem_post(&s_customer);
-            sem_post(&s_seats);
-            sem_wait(&s_barber);
-        }
-        else{
-            sem_post(&s_seats);
-            cout << "\nNo empty seats for the customer." << endl;
-        }
-        sleep(1);
+  while(1){
+    sem_wait(&s_seats);
+    if(empty > 0){
+      // when seat is available
+      empty--;
+      sem_post(&s_customer);
+      sem_post(&s_seats);
+      sem_wait(&s_barber);
+      cout<<"\nCustomer haircut Complete";
     }
+    else{
+      // when no waiting seat is available, customer has no option but to leave
+      sem_post(&s_seats);
+      cout << "\nNo empty seats for the customer." << endl;
+    }
+    sleep(rand()%2);
+  }
 }
 
 int main(){
-    pthread_t barber_thread;
-    pthread_t customers_thread;
+  cout<<"Enter no. of customer chairs\n";
+  cin>>seats;
+  empty = seats;
 
-    sem_init(&s_barber, 0, 0);
-    sem_init(&s_seats, 0, 1);
-    sem_init(&s_customer, 0, 0);
+  pthread_t thread_barber;
+  pthread_t thread_customers;
 
-    pthread_create(&barber_thread, NULL, barber, NULL);
-    pthread_create(&customers_thread, NULL, customer, NULL);
+  sem_init(&s_barber, 0, 0);
+  sem_init(&s_seats, 0, 1);
+  sem_init(&s_customer, 0, 0);
 
-    pthread_exit(NULL);
+  pthread_create(&thread_barber, NULL, barber, NULL);
+  pthread_create(&thread_customers, NULL, customer, NULL);
+
+  pthread_exit(NULL);
 }
